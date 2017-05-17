fb094d3863721fc05942cc292c68ea9a1ca2a23d
MykhailoBilous <Bilous.Mykhailo@gmail.com>
2017-05-17 18:04:48 +0300

    site_updates
    New menu, promo section added. Changes to index page

---
+++ js/menu.js
@@ -0,0 +1,8 @@
+/*     NOTES:
+ * [1] Number of list-items you want
+ * [2] Size of closed menu "icon"
+ * [3] Size of your expanded menu
+ * [4] Size of the small units
+ * [5] Used to transform the origin of the rotation on the list-itmes to the middle of the menu so they spin out in a centered circle. The same number also offsets position of the list items so they don't sit right on the edge of the actual <ul>, that way there's padding and the menu won't close down if the user moves their cursor a bit too far.
+ * [6] Used for the degrees of rotation between each list item, keeping them all evenly spaced. 360 degrees in a circle divided by the number of list items you have
+ */
